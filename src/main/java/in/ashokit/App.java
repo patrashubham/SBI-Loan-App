3baa5281a4e58e50b8af67f7fd7b0b1d1924c21f
subham <patrasubham778@gmail.com>
2024-05-24 18:05:43 +0530

    java files modified

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+int i=10;
+int j=10;
+
+       
         System.out.println( "Hello World!" );
     }
 }
--- src/test/java/in/ashokit/AppTest.java
+++ src/test/java/in/ashokit/AppTest.java
@@ -15,6 +15,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
+       int i=20;
         assertTrue( true );
     }
 }
